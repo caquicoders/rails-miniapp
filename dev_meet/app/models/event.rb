258e2eef03ebf40703df6ebb1964ad5d740c78bb
David Brusius <brusiusdavid@gmail.com>
2018-05-19 09:26:55 -0300

    Hide edit and delete buttons for event not owned by the current user

--- dev_meet/app/models/event.rb
+++ dev_meet/app/models/event.rb
@@ -4,6 +4,10 @@ class Event < ApplicationRecord
   has_many :event_attendants
   has_many :attendants, through: :event_attendants, source: :attendant
 
+  def owned_by?(user)
+    owner == user
+  end
+
   def attended_by?(user)
     attendants.exists?(user.id)
   end
